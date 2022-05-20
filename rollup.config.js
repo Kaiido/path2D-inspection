1fd6060331d11930bc125e9a9cde038109807db8
Kaiido <tristan.fraipont@gmail.com>
2022-05-20 10:53:56 +0900

    make linter happy

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,17 @@
 // rollup.config.js
-import { terser } from 'rollup-plugin-terser';
+import { terser } from "rollup-plugin-terser";
 
 export default {
-	input: './source/path2D-inspection.mjs',
-	output: [
-		{
-			file: './build/path2D-inspection.js',
-			format: 'iife'
+  input: "./source/path2D-inspection.mjs",
+  output: [
+    {
+      file: "./build/path2D-inspection.js",
+      format: "iife"
     },
-		{
-			file: './build/path2D-inspection.min.js',
-			format: 'iife',
-      plugins: [terser()]
+    {
+      file: "./build/path2D-inspection.min.js",
+      format: "iife",
+      plugins: [ terser() ]
     }
   ]
 };
