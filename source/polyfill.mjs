b41ad8f405217b0c00d77acb7929d5acb1994ba7
Kaiido <tristan.fraipont@gmail.com>
2022-11-10 10:57:20 +0900

    include svg-path-properties
    This adds 3 new methods:
     - getTotalLength()
     - getPointAtLength(length)
     - getPathSegmentAtLength(length)
    
    It also fixed the return value of getBBox to a DOMRect instead of a mere JS Object.

--- source/polyfill.mjs
+++ source/polyfill.mjs
@@ -1,10 +1,16 @@
 import PathData from "./pathdata/pathdata.mjs";
 import getBBox from "./externals/bbox/getBBox.mjs";
 import { consumers } from "./consumers.mjs";
+import {
+  svgPathProperties as PathProperties
+} from "../node_modules/svg-path-properties/dist/svg-path-properties.esm.js";
+
 let Original = globalThis.Path2D;
 const path2DMap = new WeakMap();
 const pathDataMap = new WeakMap();
 const currentPathSymbol = Symbol("currentPath");
+const noLengthSegments = new Set([ "Z", "M" ]);
+
 
 if (typeof Original.prototype.getPathData !== "function") {
   class Path2D {
@@ -26,6 +32,34 @@ if (typeof Original.prototype.getPathData !== "function") {
     }
     getBBox() {
       return getBBox(this.toSVGString());
+    }
+    getTotalLength() {
+      const properties = new PathProperties(this.toSVGString());
+      return properties.getTotalLength();
+    }
+    getPointAtLength(length) {
+      const properties = new PathProperties(this.toSVGString());
+      const { x, y } = properties.getPointAtLength(length);
+      return new DOMPoint(x, y);
+    }
+    getPathSegmentAtLength(length) {
+      const properties = new PathProperties(this.toSVGString());
+      const parts = properties.getParts();
+      const segments = this.getPathData();
+      let totalLength = 0;
+      let j = 0;
+      for (let i = 0; i < segments.length; i++) {
+        const segment = segments[i];
+        if (noLengthSegments.has(segment.type) && i < segments.length) {
+          continue;
+        }
+        const part = parts[j++];
+        totalLength += part.length;
+        if (totalLength > length) {
+          return segment;
+        }
+      }
+      return segments[segments.length - 1] || null;
     }
     get [currentPathSymbol]() {
       let path = path2DMap.get(this);
